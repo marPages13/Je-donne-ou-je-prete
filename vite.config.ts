ea67469549b72324b9b5a73c6df4c1d4c5942c05
marius <marius.pages@eduvaud.ch>
2026-03-19 11:04:14 +0100

    Trying to add env var for allowed hosts 3

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+
+import { defineConfig } from 'vite'
+import adonisjs from '@adonisjs/vite/client'
+
+console.log('ALLOWED_HOST:', process.env.ALLOWED_HOST)
+
+export default defineConfig({
+  plugins: [
+    adonisjs({
+      /**
+       * Entrypoints of your application. Each entrypoint will
+       * result in a separate bundle.
+       */
+      entrypoints: ['resources/css/app.css', 'resources/js/app.js'],
+
+      /**
+       * Paths to watch and reload the browser on file change
+       */
+      reload: ['resources/views/**/*.edge'],
+    }),
+  ],
+  server: {
+    allowedHosts: process.env.ALLOWED_HOST,
+      ? [env.ALLOWED_HOST]
+      : ['jdjp.etml.net'],
+  },
+})
