e18c5808f7d5ee84306abbf497efd99b09e34dd6
Gianmarco-Ruberti <gianmarco.ruberti@eduvaud.ch>
2026-04-21 09:30:20 +0200

    feat(choix-login) : création de la page pour choisir le type de login
    [24][DONE]

--- app/middleware/auth_middleware.ts
+++ app/middleware/auth_middleware.ts
@@ -9,7 +9,7 @@ export default class AuthMiddleware {
   /**
    * The URL to redirect to, when authentication fails
    */
-  redirectTo = '/login'
+  redirectTo = '/choix-login'
 
   async handle(
     ctx: HttpContext,
